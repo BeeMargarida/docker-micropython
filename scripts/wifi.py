11192b537d10037ad8809c238d7cb9ad0c1e7fa6
beemargarida <up201505505@gcloud.fe.up.pt>
2020-04-25 18:12:36 +0100

    Fix

---
+++ scripts/wifi.py
@@ -0,0 +1,25 @@
+import sys
+import uasyncio as asyncio
+if sys.platform != "linux":
+    from machine import Pin
+
+def connect():
+    if sys.platform != "linux":
+        led = Pin(2, Pin.OUT)
+        led.on()
+
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('--', '--')
+        # sta_if.connect('raspberrypi', 'UJr2016#')
+        while not sta_if.isconnected():
+            # await asyncio.sleep(0.5)
+            pass
+
+    print('network config:', sta_if.ifconfig())
+    if sys.platform != "linux":
+        led.off()
